c3bda9aecc0d3e200e0bba09eaa385dfa8748e78
davdav3 <dannykaganovitch57@outlook.com>
2024-08-12 15:42:49 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,40 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "deployment"
+}
 
+variable "location" {
+  description = "The location where resources will be deployed"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "aks_cluster_name" {
+  description = "The name of the AKS cluster"
+  type        = string
+  default     = "akshomeworkcluster"
+}
+
+variable "acr_name" {
+  description = "The name of the Azure Container Registry"
+  type        = string
+  default     = "acrhomeworktask"
+}
+
+variable "vm_size_one" {
+  description = "VM Size for the AKS cluster System Node Pool"
+  type        = string
+  default     = "Standard_DS2_v2"
+}
+
+variable "vm_size_two" {
+  description = "VM Size for the AKS cluster User Node Pool" 
+  type        = string
+  default     = "Standard_D2s_v3"
+}
+
+variable "azure_tenant_id" {
+  description = "The Azure Active Directory Tenant ID"
+  type        = string
+}
